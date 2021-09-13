918830d60e9e9660448c014a4c682ed893e2bf91
unknown <sofikulislameng@gmail.com>
2021-09-13 07:04:37 +0600

    my first commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const loadProducts = () => {
-  const url = `https://fakestoreapi.com/products`;
-  fetch(url)
+  // const url = `https://fakestoreapi.com/products`;
+  fetch('js/data.json')
     .then((response) => response.json())
     .then((data) => showProducts(data));
 };
@@ -15,8 +15,10 @@ const showProducts = (products) => {
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
       <div>
-    <img class="product-image" src=${image}></img>
+      <img class="product-image" src=${product.image}></img>
       </div>
+      <h3>Average-rating :${product.rating.rate}</h3>
+      <h3>Product-ID:${product.rating.count}</h3>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
@@ -37,7 +39,7 @@ const addToCart = (id, price) => {
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -46,12 +48,14 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = Math.round(total).toFixed(2);
+  updateTotal()
+
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText = Math.round(value).toFixed(2);
 };
 
 // update delivery charge and total Tax
@@ -78,3 +82,4 @@ const updateTotal = () => {
     getInputValue("total-tax");
   document.getElementById("total").innerText = grandTotal;
 };
+updateTotal()
